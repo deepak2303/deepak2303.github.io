d3fcfe39934c69538dc8258f0c037d93a300b0b7
deepak2303 <deepak.insta2002@gmail.com>
2022-10-16 18:16:09 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing-1", {
+        strings: ["Full-Stack Web Developer."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer."],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
