ba80a072b75bad28f6a270ed6dedeab94daaf9cf
deepak2303 <deepak.insta2002@gmail.com>
2023-03-01 18:43:23 +0530

    added

--- JS/script.js
+++ JS/script.js
@@ -2,3 +2,31 @@ window.addEventListener("scroll", function(){
     var header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY>0);
 })
+function openresume (){
+    window.open("https://drive.google.com/file/d/1UGjie_DLpJWqcP9MvAMrcljRKd_LCcQw/view?usp=sharing")
+}
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = function(){
+    navBar.classList.add("active");
+    menuBtn.style.opacity = "0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflow = "hidden";
+}
+cancelBtn.onclick = function(){
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+}
+let navLinks = document.querySelectorAll(".parent li a");
+for (var i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener("click" , function() {
+        navBar.classList.remove("active");
+        menuBtn.style.opacity = "1";
+        menuBtn.style.pointerEvents = "auto";
+        body.style.overflow="auto";
+    });
+}
